b6759bdb49cfff4f59da223a4e1e3931976c987e
Aitor Sanchez Cervantes <aitor614@gmail.com>
2022-08-27 00:48:44 +0200

    Creo una función y le asigno valores numericos

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+
+        suma(10, 30, 50);
+
+    }
+    public static void suma(int a, int b, int c){
+        System.out.println("La suma de la funcion es:");
+        System.out.println(a + b + c);
     }
 }
